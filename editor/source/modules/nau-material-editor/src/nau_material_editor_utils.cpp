907bdff6f00844bcda3bb8f79ca6ebf2b5e06e88
Val <valchara3103@gmail.com>
2026-03-25 22:02:29 +0300

    FIX: Update copyright year and fix formatting

--- editor/source/modules/nau-material-editor/src/nau_material_editor_utils.cpp
+++ editor/source/modules/nau-material-editor/src/nau_material_editor_utils.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 N-GINN LLC. All rights reserved.
+// Copyright 2026 N-GINN LLC. All rights reserved.
 // Use of this source code is governed by a BSD-3 Clause license that can be found in the LICENSE file.
 
 #include "nau_material_editor_utils.hpp"
--- editor/source/modules/nau-material-editor/src/nau_material_editor_utils.hpp
+++ editor/source/modules/nau-material-editor/src/nau_material_editor_utils.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 N-GINN LLC. All rights reserved.
+// Copyright 2026 N-GINN LLC. All rights reserved.
 // Use of this source code is governed by a BSD-3 Clause license that can be found in the LICENSE file.
 //
 // Nau material editor utils
@@ -14,7 +14,3 @@ public:
 
     static pxr::UsdStageRefPtr createMaterialPreviewScene();
 };
-
-
-
-
